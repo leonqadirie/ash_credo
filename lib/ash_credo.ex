26d50626250a64431811b8785aeff58d6dfa08cf
Leon Qadirie <leon.qadirie@nulltree.xyz>
2026-04-04 13:46:02 +0200

    feat: wire up AshCredo as Credo plugin with default check config

--- lib/ash_credo.ex
+++ lib/ash_credo.ex
@@ -1,18 +1,53 @@
 defmodule AshCredo do
   @moduledoc """
-  Documentation for `AshCredo`.
+  Credo checks for Ash Framework.
+
+  Provides pre-built checks that detect common Ash anti-patterns
+  by pattern matching on unexpanded source AST.
+
+  ## Plugin Usage
+
+  Add to your `.credo.exs`:
+
+      %{configs: [%{
+        name: "default",
+        plugins: [{AshCredo, []}]
+      }]}
   """
 
-  @doc """
-  Hello world.
+  import Credo.Plugin
 
-  ## Examples
-
-      iex> AshCredo.hello()
-      :world
-
+  @config_file """
+  %{
+    configs: [
+      %{
+        name: "default",
+        checks: %{
+          extra: [
+            {AshCredo.Check.Ash.MissingPrimaryKey, []},
+            {AshCredo.Check.Ash.MissingTimestamps, []},
+            {AshCredo.Check.Ash.SensitiveAttributeExposed, []},
+            {AshCredo.Check.Ash.AuthorizerWithoutPolicies, []},
+            {AshCredo.Check.Ash.MissingPrimaryAction, []},
+            {AshCredo.Check.Ash.OverlyPermissivePolicy, []},
+            {AshCredo.Check.Ash.WildcardAcceptOnAction, []},
+            {AshCredo.Check.Ash.SensitiveFieldInAccept, []},
+            {AshCredo.Check.Ash.MissingDomain, []},
+            {AshCredo.Check.Ash.NoActions, []},
+            {AshCredo.Check.Ash.EmptyDomain, []},
+            {AshCredo.Check.Ash.MissingIdentity, []},
+            {AshCredo.Check.Ash.BelongsToMissingAllowNil, []},
+            {AshCredo.Check.Ash.MissingCodeInterface, []},
+            {AshCredo.Check.Ash.LargeResource, []},
+            {AshCredo.Check.Ash.ActionMissingDescription, []}
+          ]
+        }
+      }
+    ]
+  }
   """
-  def hello do
-    :world
+
+  def init(exec) do
+    register_default_config(exec, @config_file)
   end
 end
