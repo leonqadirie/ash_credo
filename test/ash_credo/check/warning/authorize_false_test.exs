5e096b52c143c9c56bab80f12512b38464685eb8
Leon Qadirie <leon.qadirie@nulltree.xyz>
2026-04-05 22:17:24 +0200

    test: add tests for authorize false

---
+++ test/ash_credo/check/warning/authorize_false_test.exs
@@ -0,0 +1,96 @@
+defmodule AshCredo.Check.Warning.AuthorizeFalseTest do
+  use AshCredo.CheckCase
+
+  alias AshCredo.Check.Warning.AuthorizeFalse
+
+  test "reports issue for authorize?: false as direct argument" do
+    source = """
+    defmodule MyApp.Accounts do
+      def list_users do
+        Ash.read!(MyApp.User, authorize?: false)
+      end
+    end
+    """
+
+    assert [issue] = run_check(AuthorizeFalse, source)
+    assert issue.trigger == "authorize?: false"
+  end
+
+  test "reports issue for authorize?: false in a keyword list with other options" do
+    source = """
+    defmodule MyApp.Accounts do
+      def list_users(actor) do
+        Ash.read!(MyApp.User, actor: actor, authorize?: false)
+      end
+    end
+    """
+
+    assert [issue] = run_check(AuthorizeFalse, source)
+    assert issue.trigger == "authorize?: false"
+  end
+
+  test "reports issue for authorize?: false in a pipeline" do
+    source = """
+    defmodule MyApp.Accounts do
+      def list_users do
+        MyApp.User
+        |> Ash.Query.for_read(:list)
+        |> Ash.read!(authorize?: false)
+      end
+    end
+    """
+
+    assert [issue] = run_check(AuthorizeFalse, source)
+    assert issue.trigger == "authorize?: false"
+  end
+
+  test "no issue for authorize?: true" do
+    source = """
+    defmodule MyApp.Accounts do
+      def list_users do
+        Ash.read!(MyApp.User, authorize?: true)
+      end
+    end
+    """
+
+    assert [] = run_check(AuthorizeFalse, source)
+  end
+
+  test "no issue for calls without authorize? option" do
+    source = """
+    defmodule MyApp.Accounts do
+      def list_users(actor) do
+        Ash.read!(MyApp.User, actor: actor)
+      end
+    end
+    """
+
+    assert [] = run_check(AuthorizeFalse, source)
+  end
+
+  test "no issue for authorize?: false in non-Ash calls" do
+    source = """
+    defmodule MyApp.Accounts do
+      def do_thing do
+        SomeOtherLib.run(query, authorize?: false)
+      end
+    end
+    """
+
+    assert [] = run_check(AuthorizeFalse, source)
+  end
+
+  test "does not flag authorize?: false passed via a variable (known limitation)" do
+    source = """
+    defmodule MyApp.Accounts do
+      def list_users do
+        opts = [authorize?: false]
+        Ash.read!(MyApp.User, opts)
+      end
+    end
+    """
+
+    # Static analysis cannot follow variable bindings.
+    assert [] = run_check(AuthorizeFalse, source)
+  end
+end
