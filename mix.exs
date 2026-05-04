9f858dd9ec7fae8c59d7d7c29349b6754f97ca80
Leon Qadirie <leon.qadirie@nulltree.xyz>
2026-05-04 20:25:05 +0200

    chore(deps): bump deps

--- mix.exs
+++ mix.exs
@@ -43,9 +43,9 @@ defmodule AshCredo.MixProject do
       {:ash, "~> 3.0", only: [:dev, :test], runtime: false},
       {:simple_sat, "~> 0.1", only: [:dev, :test], runtime: false},
       {:credence, "~> 0.2", only: [:dev, :test], runtime: false},
-      {:ex_dna, "~> 1.3", only: [:dev, :test], runtime: false},
+      {:ex_dna, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.40", only: :dev, runtime: false},
-      {:ex_slop, "~> 0.1", only: [:dev, :test], runtime: false},
+      {:ex_slop, "~> 0.4", only: [:dev, :test], runtime: false},
       {:quokka, "~> 2.12", only: [:dev, :test], runtime: false}
     ]
   end
