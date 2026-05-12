f14773db4e14181a7fa668474526c6d166c9b14b
Leon Qadirie <leon.qadirie@nulltree.xyz>
2026-05-12 17:14:00 +0200

    chore(deps): add boxart

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule AshCredo.MixProject do
   defp deps do
     [
       {:ash, "~> 3.0", only: [:dev, :test], runtime: false},
+      {:boxart, "~> 0.3.3", only: [:dev, :test], runtime: false},
       {:credence, "~> 0.4", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", runtime: false},
       {:ex_dna, "~> 1.5", only: [:dev, :test], runtime: false},
